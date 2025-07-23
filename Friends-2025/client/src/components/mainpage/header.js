4eefe10c25896de951ac168dd9a5b650a9ea72f9
Ying <yinglu2641@gmail.com>
2025-07-23 10:18:14 -0400

    Colored dots

--- Friends-2025/client/src/components/mainpage/header.js
+++ Friends-2025/client/src/components/mainpage/header.js
@@ -1,13 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, CardGroup, CardLink, CardImg, CardBody, CardTitle, CardSubtitle, Card } from 'react-bootstrap'
-
+import './header.css'
 const Header= () => {
     return(
 <div className='header'>
 <Container className=''>
     <div class="bg-light rounded-3 text-center">
          <div class="m-4 m-lg-5">
-            <h1>F·R·I·E·N·D·S</h1>
+            <h1>F<span class="red-dot">·</span>R<span class="blue-dot">·</span>I<span class="yellow-dot">·</span>E<span class="red-dot">·</span>N<span class="yellow-dot">·</span>D<span class="blue-dot">·</span>S</h1>
             <p>React JS app with a list of characters from FRIENDS </p>
             <p>Plot: The show follows the lives of Rachel, Monica, Phoebe, Joey, Chandler, and Ross as they support each other through personal and professional challenges, often comedic and relatable. The series explores themes of friendship, love, career, and family, with storylines that resonate with audiences of all ages.  </p>
          </div>
